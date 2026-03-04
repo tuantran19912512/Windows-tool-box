262027bceb80d4ef302e200e2fbb7f0776fc313f
tuantran19912512 <josephtran2512@gmail.com>
2026-03-04 15:42:25 +0700

    Update Run.ps1

--- Run.ps1
+++ Run.ps1
@@ -1,9 +1,10 @@
-﻿
+﻿Write-Host ">>> DANG KHOI CHAY VIETTOOLBOX TU GITHUB..." -ForegroundColor Cyan
+
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
-Write-Host ">>> DANG KHOI CHAY VIETTOOLBOX TU GITHUB..." -ForegroundColor Cyan
+
 $RepoZipUrl = "https://github.com/tuantran19912512/Windows-tool-box/archive/refs/heads/main.zip"
 $ZipFile = "$env:TEMP\VietToolbox_Download.zip"
 $ExtractPath = "$env:TEMP\VietToolbox_Temp"
@@ -21,10 +22,9 @@ if ($MainScript) {
     Write-Host ">>> TOOL DANG CHAY. DUNG DONG CUA SO NAY DE TU DONG DON DEP..." -ForegroundColor Yellow
     $ToolProcess.WaitForExit()
     
-    # Dọn dẹp sau khi thoát
     Set-Location $env:TEMP
     Remove-Item $ExtractPath -Recurse -Force -ErrorAction SilentlyContinue
     Remove-Item $ZipFile -Force -ErrorAction SilentlyContinue
-    Write-Host ">>> DA DON DEP SACH SE. Bye Bye!" -ForegroundColor Green
+    Write-Host ">>> DA DON DEP SACH SE. TAM BIET!" -ForegroundColor Green
     Start-Sleep -Seconds 2
 }
