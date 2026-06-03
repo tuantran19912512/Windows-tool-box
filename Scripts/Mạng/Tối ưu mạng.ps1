c1968ac35112f68be7c092cb7acbcaac6dd9a84e
tuantran19912512 <josephtran2512@gmail.com>
2026-06-03 20:29:27 +0700

    Update Tối ưu mạng.ps1

--- Scripts/Mạng/Tối ưu mạng.ps1
+++ Scripts/Mạng/Tối ưu mạng.ps1
@@ -1,4 +1,8 @@
-﻿Add-Type -AssemblyName System.Windows.Forms
+# Khai báo hàm Ghi-Log để PowerShell hiểu
+function Ghi-Log {
+    param([string]$Message)
+    Write-Host $Message
+ Add-Type -AssemblyName System.Windows.Forms
 
 # ==============================================================================
 # SCRIPT: NETWORK TURBO ULTIMATE V4.0 - TỐI ƯU HOÁ TOÀN DIỆN MẠNG
